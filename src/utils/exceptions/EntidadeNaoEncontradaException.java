a74de39271b06fb8dbd409afb4d90f0c68de08e9
Tiago Martins Speckart <75458110+tiagospeckart@users.noreply.github.com>
2023-07-15 00:45:59 -0300

    feat: Error handling - EntidadeNaoEncontradaException
    Erro para ser implementado em todas as interfaces com retorno Optional que tentam acessar um recurso possível de ser Null

---
+++ src/utils/exceptions/EntidadeNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package utils.exceptions;
+
+public class EntidadeNaoEncontradaException extends Exception{
+    public EntidadeNaoEncontradaException(String message) {
+        super(message);
+    }
+}
